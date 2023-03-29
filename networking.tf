905cbe86c07045bcef3704a1bbfd00ad839acae9
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-03-29 11:18:24 +0300

    added public and private subnets

--- networking.tf
+++ networking.tf
@@ -1,1 +1,32 @@
+#---------------------------------------------------------------
+# Public Subnet:
+#---------------------------------------------------------------
+resource "aws_subnet" "public_subnet" {
+  depends_on = [
+    aws_vpc.test_vpc
+  ]
+  vpc_id                  = aws_vpc.test_vpc.id
+  cidr_block              = "10.10.10.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name = "Public Subnet"
+  }
+}
+
+#---------------------------------------------------------------
+# Private Subnet:
+#---------------------------------------------------------------
+resource "aws_subnet" "private_subnet" {
+  depends_on = [
+    aws_vpc.test_vpc,
+    aws_subnet.public_subnet
+  ]
+  vpc_id            = aws_vpc.test_vpc.id
+  cidr_block        = "10.10.20.0/24"
+  availability_zone = "ap-south-1b"
+  tags              = {
+    Name = "Private Subnet"
+  }
+}
 
