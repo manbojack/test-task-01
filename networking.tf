d122df36f7fa64549cdb283581e928236c7bf971
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-03-29 11:19:51 +0300

    added Route Table for the public subnet

--- networking.tf
+++ networking.tf
@@ -44,3 +44,22 @@ resource "aws_internet_gateway" "Internet_Gateway" {
     Name = "IG-Public-&-Private-VPC"
   }
 }
+
+#---------------------------------------------------------------
+# Route Table for the public subnet:
+#---------------------------------------------------------------
+resource "aws_route_table" "Public-Subnet-RT" {
+  depends_on = [
+    aws_vpc.test_vpc,
+    aws_internet_gateway.Internet_Gateway
+  ]
+  vpc_id = aws_vpc.test_vpc.id
+  # NAT Rule
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Internet_Gateway.id
+  }
+  tags = {
+    Name = "Route Table for Internet Gateway"
+  }
+}
